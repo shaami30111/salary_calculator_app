3a2c05ff5b52a365ed641c4a12ddbec35a59f021
Osama Sajid <shaami30111@gmail.com>
2023-08-16 09:51:25 +0500

    modified:   android/app/src/main/AndroidManifest.xml 	modified:   ios/Runner/Info.plist 	modified:   lib/main.dart 	deleted:    lib/pages/test.dart 	modified:   linux/CMakeLists.txt 	modified:   macos/Runner/Configs/AppInfo.xcconfig 	modified:   web/index.html 	modified:   windows/runner/Runner.rc

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:salary_cal/pages/salary_cal.dart';
 
-import 'pages/test.dart';
-
 void main() {
   runApp(const MyApp());
 }
