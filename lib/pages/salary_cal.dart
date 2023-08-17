3c4bed90c1ac361ac14d75f37504da81136de685
Osama Sajid <shaami30111@gmail.com>
2023-08-17 13:51:13 +0500

    modified:   lib/pages/salary_cal.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/pages/salary_cal.dart
+++ lib/pages/salary_cal.dart
@@ -1,7 +1,10 @@
+import 'package:age_calculator/age_calculator.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttericon/typicons_icons.dart';
+import 'package:intl/intl.dart';
+// import 'dart:io';
 
 class SalaryCal extends StatefulWidget {
   const SalaryCal({super.key});
@@ -20,11 +23,18 @@ class _SalaryCalState extends State<SalaryCal> {
   final salaryController = TextEditingController();
   final tempController = TextEditingController();
   final taxController = TextEditingController();
+  TextEditingController dateinput = TextEditingController();
+  TextEditingController enddateinput = TextEditingController();
   double netSal = 0;
   double convertedTemp = 0;
-
   String sourceUnit = 'Select Unit';
   String targetUnit = 'Select Unit';
+  String age1 = '';
+  String formattedDate = '';
+  String endformattedDate = '';
+
+  DateTime? pickedDateF;
+  DateTime? pickedDateL;
 
   final List<String> temperatureUnits = [
     "Centigrade",
@@ -33,9 +43,15 @@ class _SalaryCalState extends State<SalaryCal> {
   ];
 
   @override
+  void initState() {
+    dateinput.text = ""; //set the initial value of text field
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           leading: Icon(
@@ -65,11 +81,16 @@ class _SalaryCalState extends State<SalaryCal> {
                 icon: Icon(Icons.device_thermostat_outlined),
                 text: "Temprature Converter",
               ),
+              Tab(
+                icon: Icon(Icons.timelapse),
+                text: "Age Calculator",
+              ),
             ],
           ),
         ),
         body: TabBarView(
           children: [
+            // Salary converter
             SingleChildScrollView(
               child: SafeArea(
                 child: Padding(
@@ -519,6 +540,299 @@ class _SalaryCalState extends State<SalaryCal> {
                 ),
               ),
             ),
+            // Age calculator
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Select Date:",
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Center(
+                        child: TextField(
+                          controller:
+                              dateinput, //editing controller of this TextField
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.r),
+                              borderSide: const BorderSide(
+                                  color: Colors.transparent, width: 0),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.r),
+                              borderSide: const BorderSide(
+                                  color: Colors.transparent, width: 0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.r),
+                              borderSide: const BorderSide(
+                                  color: Colors.transparent, width: 0),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 14.h,
+                              horizontal: 20.w,
+                            ),
+                            hintStyle: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                            filled: true,
+                            fillColor: textField,
+                            hintText: "Enter Date of Birth",
+                            prefixIcon: Icon(
+                              Icons.calendar_month,
+                              size: 22.sp,
+                              color: Colors.grey,
+                            ), //icon of text field
+                          ),
+                          readOnly:
+                              true, //set it true, so that user will not able to edit text
+                          onTap: () async {
+                            pickedDateF = await showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime(
+                                    1980), //DateTime.now() - not to allow to choose before today.
+                                lastDate: DateTime(2101));
+
+                            if (pickedDateF != null) {
+                              //pickedDate output format => 2021-03-10 00:00:00.000
+                              formattedDate =
+                                  DateFormat('yyyy-MM-dd').format(pickedDateF!);
+                              print(
+                                  formattedDate); //formatted date output using intl package =>  2021-03-16
+                              //you can implement different kind of Date Format here according to your requirement
+
+                              setState(() {
+                                dateinput.text =
+                                    formattedDate; //set output date to TextField value.
+                              });
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text('Date is not selected'),
+                                  duration: Duration(
+                                      seconds:
+                                          2), // Optional: set duration for how long the SnackBar should be shown
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Text(
+                        "Select Ending Date:",
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Center(
+                        child: TextField(
+                          controller:
+                              enddateinput, //editing controller of this TextField
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.r),
+                              borderSide: const BorderSide(
+                                  color: Colors.transparent, width: 0),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.r),
+                              borderSide: const BorderSide(
+                                  color: Colors.transparent, width: 0),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.r),
+                              borderSide: const BorderSide(
+                                  color: Colors.transparent, width: 0),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 14.h,
+                              horizontal: 20.w,
+                            ),
+                            hintStyle: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                            filled: true,
+                            fillColor: textField,
+                            hintText: "Enter Date of Birth",
+                            prefixIcon: Icon(
+                              Icons.calendar_month,
+                              size: 22.sp,
+                              color: Colors.grey,
+                            ), //icon of text field
+                          ),
+                          readOnly:
+                              true, //set it true, so that user will not able to edit text
+                          onTap: () async {
+                            pickedDateL = await showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime(
+                                    1980), //DateTime.now() - not to allow to choose before today.
+                                lastDate: DateTime(2101));
+
+                            if (pickedDateL != null) {
+                              //pickedDate output format => 2021-03-10 00:00:00.000
+                              endformattedDate =
+                                  DateFormat('yyyy-MM-dd').format(pickedDateL!);
+                              print(
+                                  endformattedDate); //formatted date output using intl package =>  2021-03-16
+                              //you can implement different kind of Date Format here according to your requirement
+
+                              setState(() {
+                                enddateinput.text =
+                                    endformattedDate; //set output date to TextField value.
+                              });
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text('Date is not selected'),
+                                  duration: Duration(
+                                      seconds:
+                                          2), // Optional: set duration for how long the SnackBar should be shown
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "Age Calculated:",
+                            style: TextStyle(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      Container(
+                        height: 120.h,
+                        width: 380.w,
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              age1,
+                              style: TextStyle(
+                                fontSize: 16.sp,
+                                color: Colors.red[600],
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                            Text(
+                              age1,
+                              style: TextStyle(
+                                fontSize: 16.sp,
+                                color: Colors.red[600],
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.h,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              if (pickedDateF != null && pickedDateL != null) {
+                                ageCalOne(formattedDate, endformattedDate);
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text('Date is not selected'),
+                                    duration: Duration(
+                                        seconds:
+                                            2), // Optional: set duration for how long the SnackBar should be shown
+                                  ),
+                                );
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.r),
+                              ),
+                              backgroundColor: Color.fromARGB(255, 20, 48, 232),
+                              padding: EdgeInsets.all(12),
+                            ),
+                            child: Text(
+                              "Calculate",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          Spacer(),
+                          ElevatedButton(
+                            onPressed: () {
+                              dateinput.clear();
+                              enddateinput.clear();
+                              pickedDateF = null;
+                              pickedDateL = null;
+                              age1 = '';
+
+                              setState(() {});
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text('Cleared!'),
+                                  duration: Duration(
+                                      seconds:
+                                          2), // Optional: set duration for how long the SnackBar should be shown
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.r),
+                              ),
+                              backgroundColor: Color.fromARGB(255, 20, 48, 232),
+                              padding: EdgeInsets.all(12),
+                            ),
+                            child: Text(
+                              "Clear",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.sp,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ]),
+              ),
+            )
           ],
         ),
       ),
@@ -552,4 +866,48 @@ class _SalaryCalState extends State<SalaryCal> {
       convertedTemp = convert;
     });
   }
+
+  void ageCalOne(String date, String endD) {
+    // String current = DateFormat('yyyy-MM-dd').format(currentd);
+    // String duration =
+    //     (AgeCalculator.age(date as DateTime, today: current as DateTime))
+    //         .toString();
+    // print('Your age is $duration');
+    // age = duration;
+    // setState(() {});
+    List<String> part = date.split("-");
+    int year = int.parse(part[0]);
+    int month = int.parse(part[1]);
+    int day = int.parse(part[2]);
+    List<String> partE = endD.split("-");
+
+    int cyear = int.parse(partE[0]);
+    int cmonth = int.parse(partE[1]);
+    int cday = int.parse(partE[2]);
+    int ayear = cyear - year;
+    int amonth = cmonth - month;
+    int aday = cday - day;
+    if (cmonth < month) {
+      ayear -= 1;
+      amonth += 12;
+    }
+    if (aday < 0 && amonth != 0) {
+      amonth -= 1;
+      aday += 30;
+    }
+    if (amonth == 0 && aday < 0) {
+      aday += 30;
+      amonth = 11;
+    }
+    // print("Age:Year:$ayear\t Month:$amonth\t Day:$aday");
+    age1 = ayear.toString() +
+        " Years " +
+        amonth.toString() +
+        " Months " +
+        aday.toString() +
+        " Days ";
+    setState(() {});
+  }
+
+  void ageCalAll() {}
 }
